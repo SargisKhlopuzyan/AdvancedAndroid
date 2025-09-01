5944969bcc1fa3e6c226b321ff8be4354f380304
SargisKhlopuzyan <sargis.khlopuzyan@gmail.com>
2025-09-01 16:47:21 +0400

    Coroutine exceptions handling and Supervision (job, scope)

---
+++ app/src/main/java/com/sargis/khlopuzyan/advancedandroid/coroutine_exceptions_handling/CancellationAndExceptions.kt
@@ -0,0 +1,63 @@
+package com.sargis.khlopuzyan.advancedandroid.coroutine_exceptions_handling
+
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.NonCancellable
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+
+//fun main() = runBlocking {
+//    val job = launch {
+//        val child = launch {
+//            try {
+//                delay(Long.MAX_VALUE)
+//            } finally {
+//                println("Child is cancelled")
+//            }
+//        }
+//
+//        yield()
+//
+//        println("Cancelling child")
+//        child.cancel()
+//        child.join()
+//
+//        yield()
+//        println("Parent is not cancelled")
+//    }
+//    job.join()
+//}
+
+@OptIn(DelicateCoroutinesApi::class)
+fun main() = runBlocking {
+
+    val handler = CoroutineExceptionHandler { _, exception ->
+        println("CoroutineExceptionHandler got $exception")
+//        println("CoroutineExceptionHandler got $exception with suppressed ${exception.suppressed.contentToString()}")
+    }
+
+    val job = GlobalScope.launch(handler) {
+        launch { // the first child
+            try {
+                delay(Long.MAX_VALUE)
+            } finally {
+                withContext(NonCancellable) {
+                    println("Children are cancelled, but exception is not handled until all children terminate")
+                    delay(100)
+                    println("The first child finished its non cancellable block")
+//                    throw IndexOutOfBoundsException()
+                }
+            }
+        }
+
+        launch { // the second child
+            delay(10)
+            println("Second child throws an exception")
+            throw ArithmeticException()
+        }
+    }
+    job.join()
+}
---
+++ app/src/main/java/com/sargis/khlopuzyan/advancedandroid/supervision/SupervisionJob.kt
@@ -0,0 +1,47 @@
+package com.sargis.khlopuzyan.advancedandroid.supervision
+
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+//TODO-URL : https://kotlinlang.org/docs/exception-handling.html#supervision-job
+
+fun main() = runBlocking {
+
+    val supervisorJob = SupervisorJob()
+    val coroutineScope = CoroutineScope(coroutineContext + supervisorJob)
+
+    val coroutineExceptionHandler = CoroutineExceptionHandler { _, _ -> }
+
+    with(coroutineScope) {
+
+        // launch the first child -- its exception is ignored for this example (don't do this in practice!)
+        val firstChild = launch(coroutineExceptionHandler) {
+            println("The first child is failing")
+            throw AssertionError("The first child is cancelled")
+        }
+
+        // launch the second child
+        val secondChild = launch {
+            firstChild.join()
+
+            // Cancellation of the first child is not propagated to the second child
+            println("The first child is cancelled: ${firstChild.isCancelled}, but the second one is still active")
+            try {
+                delay(Long.MAX_VALUE)
+            } finally {
+                // But cancellation of the supervisor is propagated
+                println("The second child is cancelled because the supervisor was cancelled")
+            }
+        }
+
+        // wait until the first child fails & completes
+        firstChild.join()
+        println("Cancelling the supervisor")
+        supervisorJob.cancel()
+        secondChild.join()
+    }
+}

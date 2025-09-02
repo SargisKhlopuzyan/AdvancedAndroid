d3c6684e9e9402547925a2cf6024446ec51da01e
SargisKhlopuzyan <sargis.khlopuzyan@gmail.com>
2025-09-02 21:36:31 +0400

    Coroutines in depth

---
+++ app/src/main/java/com/sargis/khlopuzyan/advancedandroid/coroutines_in_depth/CoroutinesInDepth.kt
@@ -0,0 +1,59 @@
+package com.sargis.khlopuzyan.advancedandroid.coroutines_in_depth
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlin.coroutines.coroutineContext
+import kotlin.system.measureTimeMillis
+
+//fun main() = runBlocking {
+//    val timeMillis = measureTimeMillis {
+//        println("Fetching dashboard data...")
+//        val userDeferred = async { fetchUserData() }
+//        val notificationsDeferred = async { fetchNotificationCount() }
+//        // Wait for both results to complete
+//        val userData = userDeferred.await()
+//        val notifications = notificationsDeferred.await()
+//        println("Dashboard Data: $userData, Notifications: $notifications")
+//    }
+//
+//    println("timeMillis: $timeMillis")
+//}
+
+
+fun main() = runBlocking {
+    val timeMillis = measureTimeMillis {
+        val dispatcher = coroutineContext[CoroutineDispatcher]
+
+        println("runBlocking, thread: ${Thread.currentThread()}")
+        println("runBlocking, dispatcher: $dispatcher")
+        println("Fetching dashboard data...")
+
+        val userDeferred = launch { fetchUserData() }
+        val notificationsDeferred = launch { fetchNotificationCount() }
+        // Wait for both results to complete
+        listOf(userDeferred, notificationsDeferred).joinAll()
+    }
+
+    println("timeMillis: $timeMillis")
+}
+
+suspend fun fetchUserData(): String {
+    delay(1000) // Simulate API call
+    val dispatcher = coroutineContext[CoroutineDispatcher]
+    println("fetchUserData, thread: ${Thread.currentThread()}")
+    println("fetchUserData, dispatcher: $dispatcher")
+    return "User: Jane Doe"
+}
+
+suspend fun fetchNotificationCount(): Int {
+    delay(500) // Simulate API call
+    val dispatcher = coroutineContext[CoroutineDispatcher]
+    println("fetchNotificationCount, thread: ${Thread.currentThread()}")
+    println("fetchNotificationCount, dispatcher: $dispatcher")
+    return 5
+}

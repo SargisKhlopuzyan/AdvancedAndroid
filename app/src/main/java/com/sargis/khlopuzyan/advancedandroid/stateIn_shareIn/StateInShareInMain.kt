1060d87c34411558940eaa39bc327e31593fffc2
SargisKhlopuzyan <sargis.khlopuzyan@gmail.com>
2025-09-02 19:33:53 +0400

    stateIn and shareIn

---
+++ app/src/main/java/com/sargis/khlopuzyan/advancedandroid/stateIn_shareIn/StateInShareInMain.kt
@@ -0,0 +1,19 @@
+package com.sargis.khlopuzyan.advancedandroid.stateIn_shareIn
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    val stateFlow = returnsFlow().stateIn(this, SharingStarted.Lazily, 0)
+    val sharedFlow = returnsFlow().shareIn(this, SharingStarted.Lazily)
+}
+
+private fun returnsFlow(): Flow<Int> = flow {
+    (0..10).forEach { index ->
+        emit(index)
+    }
+}

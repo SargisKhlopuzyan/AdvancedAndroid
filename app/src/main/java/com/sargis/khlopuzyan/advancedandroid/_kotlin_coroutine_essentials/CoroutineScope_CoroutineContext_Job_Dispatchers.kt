f2a9d964514a37984c01738970002e265c0caf47
SargisKhlopuzyan <sargis.khlopuzyan@gmail.com>
2025-09-12 15:47:08 +0400

    Kotlin Coroutine Essentials: CoroutineScope, CoroutineContext, Job, Dispatchers

---
+++ app/src/main/java/com/sargis/khlopuzyan/advancedandroid/_kotlin_coroutine_essentials/CoroutineScope_CoroutineContext_Job_Dispatchers.kt
@@ -0,0 +1,59 @@
+package com.sargis.khlopuzyan.advancedandroid._kotlin_coroutine_essentials
+
+import kotlinx.coroutines.CompletableJob
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.async
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    runBlocking {
+        println("Start")
+
+//        val context = Job() +
+        val context = SupervisorJob() +
+                Dispatchers.Default +
+                CoroutineName("Test") +
+                CoroutineExceptionHandler { context, throwable -> }
+
+        val supervisorContext = SupervisorJob() +
+                Dispatchers.Default +
+                CoroutineName("Test") +
+                CoroutineExceptionHandler { context, throwable -> }
+
+//        val completableContext = CompletableJob() +
+//                Dispatchers.Default +
+//                CoroutineName("Test") +
+//                CoroutineExceptionHandler { context, throwable -> }
+
+        val scope = CoroutineScope(context)
+
+        scope.launch {
+            launch {
+
+            }
+        }
+
+        scope.async { }
+
+        val job = scope.async {
+            delay(1000)
+            println("Coroutine completed")
+            1
+        }
+
+        scope.cancel()
+
+        val result = job.await()
+        println("result: $result")
+    }
+
+    println("runBlocking ends")
+}
